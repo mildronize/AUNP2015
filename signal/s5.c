d557affb0e8de8c055a26adc8c44a8a7a8e6d11f
Chatchai J. AUNP 2015 <aunp2015@cjv6.net>
2015-11-11 13:59:19 +0700

    yesterday

--- signal/s5.c
+++ signal/s5.c
@@ -8,19 +8,39 @@ void	sighandler(int signo) {
 	printf("Got signal %d\n", signo);
 }
 
-int	main(void) {
-	int	count = 0;
+void	do_something(char *s) {
+	int	i;
+	for (i=0;i<5;i++) { fprintf(stderr, "do_something: %s %d\n", s, i); sleep(1); }
+}
 
+int	main(void) {
 	struct	sigaction oldact, act;
+
+	/* ---- signal() ----- */
+
+	signal(SIGINT, SIG_IGN);
+	do_something("signal");
+
+/*
+	signal(SIGINT, SIG_DFL);
+	do_something("after signal()");
+*/
+	signal(SIGINT, &sighandler);
+
+	/* --- sigaction() ---- */
 
 	sigaction(SIGINT, NULL, &oldact);
 	act = oldact;
 	act.sa_handler = SIG_IGN;
 	sigaction(SIGINT, &act, NULL);
+
+	do_something("sigaction");
+
+	sigaction(SIGINT, &oldact, NULL);
+
+	do_something("after sigaction()");
+
+	/* end sigaction */
 	
-	while (1) {
-		printf("count = %d\n", count++);
-		sleep(1);
-	}
 	return 0;
 }
